c83a3f2239d9d9c5ba73aa87ccd4895c01221840
Ansari Ahmed Faraz <ahmedfaraz.ansari@bakerhughes.com>
2023-01-28 19:12:39 +0530

    Modified image sizes due to available hardware and updated checkpoint resume code to non strict

--- datasets/coco.py
+++ datasets/coco.py
@@ -119,17 +119,17 @@ def make_coco_transforms(image_set):
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-    scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+    scales = [480, 512]
 
     if image_set == 'train':
         return T.Compose([
             T.RandomHorizontalFlip(),
             T.RandomSelect(
-                T.RandomResize(scales, max_size=1333),
+                T.RandomResize(scales, max_size=512),
                 T.Compose([
-                    T.RandomResize([400, 500, 600]),
-                    T.RandomSizeCrop(384, 600),
-                    T.RandomResize(scales, max_size=1333),
+                    T.RandomResize([400, 500]),
+                    T.RandomSizeCrop(384, 480),
+                    T.RandomResize(scales, max_size=512),
                 ])
             ),
             normalize,
@@ -137,7 +137,7 @@ def make_coco_transforms(image_set):
 
     if image_set == 'val':
         return T.Compose([
-            T.RandomResize([800], max_size=1333),
+            T.RandomResize([400], max_size=512),
             normalize,
         ])
 
